b8dc4e64c6c0fa0e2dbf34efa86f375fbc801f20
psy0524 <parksangy@naver.com>
2024-05-25 17:48:45 +0900

    원상태

--- stage1.py
+++ stage1.py
@@ -44,6 +44,19 @@ class Block:
         self.x = x
         self.y = y
 
+# 움직이는 블록 클래스 정의 (Block 클래스 상속)
+class MovingBlock(Block):
+    def __init__(self, x, y, move_range=100, speed=2):
+        super().__init__(x, y)
+        self.move_range = move_range
+        self.speed = speed
+        self.initial_x = x
+        self.direction = 1
+
+    def move(self):
+        self.x += self.speed * self.direction
+        if self.x > self.initial_x + self.move_range or self.x < self.initial_x - self.move_range:
+            self.direction *= -1
 # 블록 리스트 초기화
 blocks = [Block(x, y) for x, y in blocks_positions]
 
